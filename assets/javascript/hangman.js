e72ab66eda495e482ccf015ea2cdaffc7a210c1d
cslewislives <cslewislives@gmail.com>
2017-12-02 12:17:04 -0500

    Pseudo Code

---
+++ assets/javascript/hangman.js
@@ -0,0 +1,17 @@
+//Press any key to get started!
+
+//display spaces
+
+//user inputs letters
+
+//display wrong and right guesses
+
+//display guesses left
+
+//show wins count and/or victory screen if word guessed correctly
+
+//give a max try amount until fail.
+
+//show fail screen if fail.
+
+//try again functionality, multiple words to cycle through.
