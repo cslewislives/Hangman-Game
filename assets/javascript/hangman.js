2572983011fb3792b3fd2e710f3871aea494d687
cslewislives <cslewislives@gmail.com>
2017-12-06 03:41:17 -0500

    Game Logic Finished

--- assets/javascript/hangman.js
+++ assets/javascript/hangman.js
@@ -3,33 +3,41 @@ var alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"; // Variable storing the alphabet
 var alphArr = alphabet.split("");
 console.log(alphArr);
 //a list of words from the theme for the computer to choose from after getting started.
-var words = [
-  "Harry",
-  "Ron",
-  "Hermione",
-  "Ginny",
-  "Malfoy",
-  "Dumbledore",
-  "Hagrid",
-  "Gryffindor",
-  "Hufflepuff",
-  "Ravenclaw",
-  "Slytherin",
-  "Hogwarts"
-];
-var sorted = []; //turn all words in the array into uppercase
-for (var i = 0; i < words.length; i++) {
-  sorted[i] = words[i].toUpperCase();
+function chooseWord() {
+
+    var words = [
+        "Harry",
+        "Ron",
+        "Hermione",
+        "Ginny",
+        "Malfoy",
+        "Dumbledore",
+        "Hagrid",
+        "Gryffindor",
+        "Hufflepuff",
+        "Ravenclaw",
+        "Slytherin",
+        "Hogwarts"
+    ];
+    var sorted = []; //turn all words in the array into uppercase
+    for (var i = 0; i < words.length; i++) {
+        sorted[i] = words[i].toUpperCase();
+    }
+    console.log(sorted);
+    chosenWord = sorted[Math.floor(Math.random() * sorted.length)];
+    console.log(chosenWord);
+    length = chosenWord.length;
 }
-console.log(sorted);
-var chosenWord = sorted[Math.floor(Math.random() * sorted.length)];
-console.log(chosenWord);
+
+chooseWord();
+var chosenWord;
 var spacesLeft = [];
 var wrong = " ";
 var guesses = []; //empty array for storing user guesses
-var lives = 13;
+var lives = 7;
 var wins = 0;
-var length = chosenWord.length;
+var losses = 0;
+var length;
 var remaining = length;
 console.log("Remaining Letters " + remaining);
 
@@ -42,15 +50,15 @@ document.getElementById("word").innerHTML =
   "<h2>" + spacesLeft.join(" ") + "</h2>";
 console.log(spacesLeft);
 
-//set a variable for remaining letters
-
 //Press any key to get started!
 //replace heading with the spaces of the chosen word
 document.onkeyup = function(event) {
+    
   document.getElementById("hide").style.display = "none";
   document.getElementById("guesses").innerHTML = "Guesses: ";
   document.getElementById("lives").innerHTML = "Guesses Remaining: " + lives;
   document.getElementById("wins").innerHTML = "Wins: " + wins;
+  document.getElementById("losses").innerHTML = "Losses: " + losses;
 
   var userGuess = event.key.toUpperCase();
   console.log("chosenWord Index " + chosenWord.indexOf(userGuess));
@@ -75,7 +83,7 @@ document.onkeyup = function(event) {
         }
       } else if (chosenWord.indexOf(userGuess) == -1) {
         //show incorrect guess
-        wrong += ", " + userGuess;
+        wrong += userGuess + " ";
         document.getElementById("wrongLetters").innerHTML = wrong;
         //lose a life
         document.getElementById("lives").innerHTML =
@@ -90,30 +98,65 @@ document.onkeyup = function(event) {
   //If word is completed {
   if (remaining === 0) {
     //Show victory Screen
-    document.getElementById("victory").innerHTML =
-      "<h2>" + "Congrats!" + "</h2>";
+    document.getElementById("victory").style.display = "block";
+    document.getElementById("victory").innerHTML = "You've Won the House Cup!";
     //hide wrong guesses
+    document.getElementById("guesses").style.display = "none";
+    
     document.getElementById("wrongLetters").style.display = "none";
     //update win count
     document.getElementById("wins").innerHTML = "Wins: " + ++wins;
     //Try Again?
-    document.getElementById("lives").innerHTML =
-      "<button>" + "New Word?" + "</button>";
+    document.getElementById("lives").style.display = "none";
+    document.getElementById("resetButton").style.display = "block";
   } else if (lives === 0) {
     //give a max try amount until fail.
     //If last life is lost {
     //Show failure screen
-    document.getElementById("victory").innerHTML =
-      "<h2>" + "You've Been Cursed!" + "</h2>";
+    document.getElementById("defeat").style.display = "block";
+    
+    document.getElementById("defeat").innerHTML = "You've Been Cursed!";
     //hide wrong guesses
+    document.getElementById("guesses").style.display = "none";
+    
     document.getElementById("wrongLetters").style.display = "none";
     //Try Again?
-    document.getElementById("lives").innerHTML =
-      "<button>" + "New Word?" + "</button>";
+    document.getElementById("lives").style.display = "none";
+    document.getElementById("resetButton").style.display = "block";
+    document.getElementById("losses").innerHTML = "Losses: " + ++losses;
   }
 };
 
+guesses = [];
+
 //Reset game
+function reset() {
+    chooseWord();
+    wrong = " ";
+    remaining = length;
+    lives = 7;
+    // guesses = [];
+    spacesLeft = [];
+    for (var j = 0; j < length; j++) {
+        //Creating the correct amount of spaces for the chosen word
+        spacesLeft[j] = " _ ";
+      }
+    document.getElementById("word").innerHTML =
+    "<h2>" + spacesLeft.join(" ") + "</h2>";
+    document.getElementById("victory").style.display = "none";
+    document.getElementById("defeat").style.display = "none";
+    document.getElementById("wrongLetters").style.display = "block";
+    document.getElementById("wrongLetters").innerHTML = wrong;
+    document.getElementById("guesses").style.display = "block";
+    document.getElementById("lives").style.display = "block";
+    document.getElementById("lives").innerHTML = "Guesses Remaining: " + lives;
+    // document.getElementById("wins").innerHTML = "Wins: " + wins;
+    document.getElementById("resetButton").style.display = "none";
+    return chosenWord;
+}
+
+var newWord = chosenWord;
+console.log(newWord);
 //If try again {
 //reset the game
 //}
